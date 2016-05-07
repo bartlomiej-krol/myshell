e130f691b45d099506cba60703accadd2db5214b
bartlomiej-krol <bartlomiej_krol@interia.pl>
2016-05-08 00:29:42 +0200

    ExitCommand class created.

---
+++ src/myshell/ExitCommand.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myshell;
+
+/**
+ *
+ * @author Branimir
+ */
+class ExitCommand implements Command {
+
+    public ExitCommand() {
+    }
+
+    @Override
+    public void execute(Shell shell, String[] params) throws NotEnoughParametersException {
+        shell.setWorking(false);
+    }
+    
+}
