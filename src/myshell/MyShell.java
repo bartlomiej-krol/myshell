f777ff96f50d2fcd5e53f9577242dabfadb2a769
bartlomiej-krol <bartlomiej_krol@interia.pl>
2016-05-07 22:30:18 +0200

    MyShellClass created.

--- src/myshell/MyShell.java
+++ src/myshell/MyShell.java
@@ -5,17 +5,57 @@
  */
 package myshell;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
- * @author Branimir
+ * @author Bartłomiej Król
  */
-public class MyShell {
-
+public class MyShell implements Shell {
+    static final String APP_NAME = "MyShell";
+    static final String DEFAULT_PROMPT = "$";
+    private Boolean cwdPromptEnabled = false;
+    private String currentPrompt;
+    private Map<String,Command> commands;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Shell myshell = new MyShell();
+        System.out.println("Hello World");
+        myshell.getCommands().get("prompt").execute(myshell, args);
+        System.out.println(myshell.getCurrentPrompt());
     }
+    
+    public MyShell(){
+        init();
+        loadCommands();
+    }
+    
+    private void init() {
+        setCurrentPrompt(DEFAULT_PROMPT);
+    }
+    
+    private void loadCommands(){
+        this.commands = new HashMap<>();
+        this.commands.put("prompt", new PromptCommand());
+    }
+    
+    @Override
+    public Map<String,Command> getCommands(){
+        return this.commands;
+    }
+
+    @Override
+    public void setCurrentPrompt(String prompt) {
+       this.currentPrompt = prompt;
+    }
+
+    @Override
+    public String getCurrentPrompt() {
+        return this.currentPrompt;
+    }
+    
     
 }
